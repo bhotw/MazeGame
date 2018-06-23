c8700ee6e1d1df0f9cbe2898c92ccba641357958
Md Sheik <madsheik633@hotmail.com>
2018-06-23 03:22:34 -0400

    added the new directions to the main.py

--- MazeGame-master/main.py
+++ MazeGame-master/main.py
@@ -1,6 +1,11 @@
 import maze 
 maze.start_maze("maze2.txt")
-list_dir = [ "left", "right", "up", "down"]
+list_dir = { 
+	"a" : "left",
+	"d" : "right",
+	"w" : "up",
+	"s" : "down"
+	}
 
 def check_valid_move(x):
 	y = maze.look_in_direction(x)
@@ -8,6 +13,7 @@ def check_valid_move(x):
 		return False
 	else:
 		return True 
+
 maz_character_to_description = {
 	"X" : "a wall",
 	"K" : "a key" ,
@@ -18,8 +24,27 @@ maz_character_to_description = {
 	"C" : "coin",
 	"W" : "a sword"
 }	
+
+
+
+def direction():
+	main_space = maze.get_current_space()
+	for i in list_dir:
+		if user_input == i:
+			if check_valid_move(list_dir[i]):
+				maze.move(list_dir[i])
+				maze.look_around()
+			else:
+				print("There is a wall/!")
+
 def look():
 	print ( maz_character_to_description[maze.get_current_space()])
+
+def maps():
+	if "M" in maze.get_inventory():
+			maze.print_maze()
+	else:
+		print ("you don't have a map!")
 
 
 def add_to_inventory():
@@ -57,31 +82,35 @@ def open_door():
 		print("There is no door what the hack do you want me to open? dumb...dumb...")
 		maze.look_around()
 
-
 def kill():
 	curntLocation = maze.get_current_space()
 	if curntLocation == "S":
-		for s in maze.get_inventory():
-			if s == "W":
-				print ("You have killed the snake!")
-				maze.get_inventory().remove(s)
+		if "W" in maze.get_inventory():
+			print ("You have killed the snake!")
+			maze.get_inventory().remove("W")
+			maze.remove_item_from_space()
 		else:
-			print ("you don't have anything to kill the snake with.")
+			print ("you don't have anything to kill the snake. So you are dead")
+			return False
 	else:
 		print ("there is no snake what the hack do you want me to kill?")
 		
 def bribe():
 	curntLocation = maze.get_current_space()
 	if curntLocation == "S":
-		for c in maze.get_inventory():
-			if c == "C":
-				print ("you have bribed the snake!")
-				maze.get_inventory().remove(c)
-			else:
-				print ("You don't have anything the bribe the snake")
+		if "C" in maze.get_inventory():
+			print ("you have bribed the snake!")
+			maze.get_inventory().remove("C")
+		else:
+			print ("You don't have anything the bribe the snake")
 	else:
 		print ("there is no snake, dummy.")
+
+
 	
+
+
+
 
 
 
@@ -90,24 +119,22 @@ while True :
 	if user_input == "look around":
 		maze.look_around()
 	elif user_input in list_dir:
-		if check_valid_move(user_input):
-			maze.move(user_input)
-			maze.look_around()
-		else:
-			print("There is a wall! the player can not move.")
+		direction()
 	elif user_input == "look":
 		look()
-	elif user_input == "pick up":
+	elif user_input == "e":
 		add_to_inventory()
 	elif user_input == "inventory":
 		print(maze.get_inventory())
-	elif user_input == "map":
-		maze.print_maze()
-	elif user_input == "open":
+	elif user_input == "m":
+		maps()
+	elif user_input == "o":
 		if open_door() == False:
 			break
-	elif user_input == "kill":
-		kill()
-	elif user_input == "bribe":
+	elif user_input == "k":
+		if kill() == False:
+			break
+	elif user_input == "b":
 		bribe()
-		
+	
+	
